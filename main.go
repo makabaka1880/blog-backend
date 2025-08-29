5b23784d20d640a62fa7dc00cbfdf7255f37c9c5
makabaka1880 <makabaka1880@outlook.com>
2025-08-29 22:49:48 +0800

    updated router shadowing and cors config

--- main.go
+++ main.go
@@ -11,11 +11,14 @@ var router *gin.Engine
 
 func main() {
 	initDB()
-	router := gin.Default()
+	router = gin.Default() // fixed
+
 	router.Use(cors.New(cors.Config{
 		AllowOrigins: []string{
-			"https://blog.makabaka1880.xyz",
-			"https://api.blog.makabaka1880.xyz",
+			"http://localhost:5173",         // dev
+			"http://localhost:8080",         // dev (backend UI)
+			"http://blog.makabaka1880.xyz",  // prod http
+			"https://blog.makabaka1880.xyz", // prod https
 		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
@@ -23,9 +26,11 @@ func main() {
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
+
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"res": "pong"})
 	})
+
 	configPostsRoute(router)
 	router.Run(":8080")
 }
